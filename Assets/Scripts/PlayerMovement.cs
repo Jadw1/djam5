4f6aeb7a0b051bbe301cba63639885abbffbbde4
Yarde <dawidkal66@gmail.com>
2021-01-02 12:46:30 +0100

    dimentions changed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,22 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     private Animator _animator;
-    private Rigidbody2D _rigidBody;
-    private Vector2 _movement;
+    private Rigidbody _rigidBody;
+    private Vector3 _movement;
     private static readonly int Horizontal = Animator.StringToHash("Horizontal");
     private static readonly int Vertical = Animator.StringToHash("Vertical");
     private static readonly int Speed = Animator.StringToHash("Speed");
 
     private void Start()
     {
-        _rigidBody = GetComponent<Rigidbody2D>();
+        _rigidBody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         _movement.x = Input.GetAxis("Horizontal");
-        _movement.y = Input.GetAxis("Vertical");
+        _movement.z = Input.GetAxis("Vertical");
         
         _animator.SetFloat(Horizontal, _movement.x);
         _animator.SetFloat(Vertical, _movement.y);
