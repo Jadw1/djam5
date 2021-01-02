23d659315a2292d07b45916c5e3e4293a9465950
Jadw1 <jadw1_1@o2.pl>
2021-01-02 15:57:50 +0100

    now player is following

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
     public Color hitColor = Color.red;
@@ -10,16 +12,30 @@ public class Enemy : MonoBehaviour
     
     private Color _targetColor;
     private Renderer _renderer;
+    private NavMeshAgent _agent;
 
-    private void Start()
-    {
+    private Transform _player;
+
+    private void Start() {
+        _agent = GetComponent<NavMeshAgent>();
         _renderer = GetComponent<Renderer>();
         _targetColor = _renderer.material.color;
+
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     private void Update()
     {
+        GoToPlayer();
         _renderer.material.color = Color.Lerp(_renderer.material.color, _targetColor, lerpFactor * Time.deltaTime);
+    }
+
+    private void GoToPlayer() {
+        if (!_player) {
+            return;
+        }
+
+        _agent.SetDestination(_player.position);
     }
 
     public void DoDamage(float amount)
