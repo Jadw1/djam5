69d1d3378d89b4f6289b13b9c4398304f0884684
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2021-01-03 21:41:34 +0100

    add points calculation

---
+++ Assets/Scripts/PointsManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PointsManager : MonoBehaviour
+    {
+        private static PointsManager _instance;
+        public static PointsManager Instance => _instance;
+
+        private int _points = 0;
+        public int Points => _points;
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (this == _instance)
+            {
+                _instance = null;
+            }
+        }
+
+        public void AddPoint()
+        {
+            _points++;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/DisplayPoints.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DefaultNamespace;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class DisplayPoints : MonoBehaviour
+    {
+        private TextMeshProUGUI _text;
+        private int _lastPoints = 0;
+
+        private void Start()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void FixedUpdate()
+        {
+            var points = PointsManager.Instance.Points;
+            
+            if (points != _lastPoints)
+            {
+                _lastPoints = points;
+                _text.SetText(points.ToString());
+            }
+        }
+    }
+}
